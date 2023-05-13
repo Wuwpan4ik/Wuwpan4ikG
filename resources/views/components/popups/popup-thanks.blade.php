b5b850efde6924dccded23830b11b975c28214e2
unknown <saquiter@inbox.ru>
2023-05-13 19:24:12 +0300

    migrations and styles

---
+++ resources/views/components/popups/popup-thanks.blade.php
@@ -0,0 +1,20 @@
+<head>
+  <link rel="stylesheet" href="{{ asset('css/thank-you.css') }}">
+</head>
+<div class="box">
+  <div class="wrapper">
+    <div class="circle-blue">
+      <div class="circle-white"></div>
+    </div>
+    <svg class="check-svg" width="70" height="70" viewBox="0 0 70 70">
+      <polyline class="check-poly" points="23 34 34 43 47 27" stroke-linecap="round" />
+    </svg>
+  </div>
+  <div class="boxtext">
+    <div class="title">СПАСИБО!</div> 
+    <div class="text"> Оплата прошла успешно</div>
+  </div>
+   <!--  <button class="btn">Перейти к чату</button> -->
+</div>
+
+<script src="{{asset('js/thank-you.js')}}"></script>
